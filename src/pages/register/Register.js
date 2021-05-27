a1e33ecb300ffe848b8e2ce7e642c7ede24650b2
Ali <Ali@abc.com>
2021-05-28 03:55:05 +0500

    Footer Added To Register Page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavbarComp from "../../components/homeComponent/navbar/NavbarComp";
 import Header from '../../components/header/Header'
 import RegForm from '../../components/registerPageComp/regForm/RegForm';
+import Footer from '../../components/footer/Footer';
 
 const Register = () => {
     return (
@@ -9,6 +10,7 @@ const Register = () => {
             <NavbarComp/>
             <Header/>
             <RegForm/>
+            <Footer/>
         </div>
     )
 }
